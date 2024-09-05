4d94aaef94ba7d2a5928bec528a9adf09088721f
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2024-09-06 00:03:30 +0100

    feat: image and thread

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { AtpAgent } from "@atproto/api";
+import axios from "axios";
 import * as dotenv from "dotenv";
 import { scheduleJob } from "node-schedule";
 import * as process from "node:process";
@@ -21,23 +22,74 @@ async function start() {
 		.end(async (err, res) => {
 			if (err) return 0;
 			if (res.body.head.length > 0)
-				await agent
-					.post({
-						text: `🛫 ${res.body.head.length > 10 ? "10" : res.body.head.length} TRENDING TOPICS PARA VOCÊ ACOMPANHAR:\n\n${res.body.data
-							.sort((a, b) => b.count - a.count)
-							.slice(0, 10)
-							.map(
-								(element, i) =>
-									`${i + 1}. ${element.text} - ${new Intl.NumberFormat("en", { notation: "compact" }).format(element.count)}`,
-							)
-							.join("\n")}`,
-						langs: ["pt"],
-					})
-					.then(async (element) => {
-						await agent.like(element.uri, element.cid);
-					})
-					.catch((err) => {
-						return console.log(err);
+				await axios
+					.post(
+						"https://prismatic-squirrel-9e8dca.netlify.app/api/cards/trendingTT",
+						res.body.data.sort((a, b) => b.count - a.count).slice(0, 10),
+						{
+							responseType: "text",
+							responseEncoding: "base64",
+						},
+					)
+					.then(async (response) => {
+						const buffer = Buffer.from(response.data, "base64");
+						const { data } = await agent.uploadBlob(buffer);
+						await agent
+							.post({
+								text: `🛫 ${res.body.head.length > 10 ? "10" : res.body.head.length} TRENDING TOPICS PARA VOCÊ ACOMPANHAR:\n\n${res.body.data
+									.sort((a, b) => b.count - a.count)
+									.slice(0, 5)
+									.map(
+										(element, i) =>
+											`${i + 1}. ${element.text} - ${new Intl.NumberFormat("en", { notation: "compact" }).format(element.count)} posts`,
+									)
+									.join("\n")} ++`,
+								langs: ["pt"],
+								embed: {
+									$type: "app.bsky.embed.images",
+									images: [
+										{
+											alt: "10 tendências listadas por imagem.",
+											image: data.blob,
+											aspectRatio: {
+												width: 800,
+												height: 1900,
+											},
+										},
+									],
+								},
+							})
+							.then(async (element) => {
+								await agent.like(element.uri, element.cid);
+								await agent
+									.post({
+										text: `${res.body.data
+											.sort((a, b) => b.count - a.count)
+											.slice(5, 10)
+											.map(
+												(element, i) =>
+													`${i + 6}. ${element.text} - ${new Intl.NumberFormat("en", { notation: "compact" }).format(element.count)} posts`,
+											)
+											.join("\n")}`,
+										langs: ["pt"],
+										reply: {
+											root: {
+												uri: element.uri,
+												cid: element.cid,
+											},
+											parent: {
+												uri: element.uri,
+												cid: element.cid,
+											},
+										},
+									})
+									.then(async (post2) => {
+										await agent.like(post2.uri, post2.cid);
+									});
+							})
+							.catch((err) => {
+								return console.log(err);
+							});
 					});
 		});
 }
