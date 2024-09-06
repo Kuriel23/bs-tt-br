ef40913a8e1cdfe7322337e39d8f8d049a65110f
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2024-09-06 17:48:42 +0100

    feat: replace superagent by axios fixes: login out of start function

--- index.js
+++ index.js
@@ -3,27 +3,26 @@ import axios from "axios";
 import * as dotenv from "dotenv";
 import { scheduleJob } from "node-schedule";
 import * as process from "node:process";
-import superagent from "superagent";
 
 dotenv.config();
 
-const agent = new Bot({ langs: ["pt"] });
+const agent = new Bot({ langs: ["pt"] }).login({
+	identifier: process.env.BLUESKY_USERNAME,
+	password: process.env.BLUESKY_PASSWORD,
+});
 
-async function start() {
-	await agent.login({
-		identifier: process.env.BLUESKY_USERNAME,
-		password: process.env.BLUESKY_PASSWORD,
-	});
-
-	superagent
+function start() {
+	axios
 		.get("https://betterbluesky.nemtudo.me/api/trends")
-		.end(async (err, res) => {
-			if (err) return 0;
-			if (res.body.head.length > 0)
+		.catch(() => {
+			return 0;
+		})
+		.then(async (res) => {
+			if (res.data.head.length > 0)
 				await axios
 					.post(
 						"https://prismatic-squirrel-9e8dca.netlify.app/api/cards/trendingTT",
-						res.body.data.slice(0, 10),
+						res.data.data.slice(0, 10),
 						{
 							responseType: "text",
 							responseEncoding: "base64",
@@ -32,9 +31,9 @@ async function start() {
 					.then(async (response) => {
 						const buffer = Buffer.from(response.data, "base64");
 						const richText = new RichText().text(
-							`🛫 ${res.body.head.length > 10 ? "10" : res.body.head.length} TRENDING TOPICS PARA VOCÊ ACOMPANHAR:\n\n`,
+							`🛫 ${res.data.head.length > 10 ? "10" : res.data.head.length} TRENDING TOPICS PARA VOCÊ ACOMPANHAR:\n\n`,
 						);
-						res.body.data.slice(0, 5).map((element, i) => {
+						res.data.data.slice(0, 5).map((element, i) => {
 							richText.text(`${i + 1}. `);
 
 							element.text.startsWith("#")
@@ -69,7 +68,7 @@ async function start() {
 								await element.like();
 
 								const richText2 = new RichText();
-								res.body.data.slice(5, 10).map((element, i) => {
+								res.data.data.slice(5, 10).map((element, i) => {
 									richText2.text(`${i + 6}. `);
 
 									element.text.startsWith("#")
@@ -91,6 +90,7 @@ async function start() {
 									})
 									.then(async (post2) => {
 										await post2.like();
+										console.log("✅ [GATEWAY] • Reproduzido com sucesso");
 									});
 							})
 							.catch((err) => {
@@ -105,5 +105,4 @@ console.log("✅ [GATEWAY] • Iniciado");
 scheduleJob("*/30 * * * *", async () => {
 	console.log("❇️ [GATEWAY] • Reproduzindo novo post");
 	start();
-	console.log("✅ [GATEWAY] • Reproduzido com sucesso");
 });
